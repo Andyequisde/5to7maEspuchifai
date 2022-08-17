b536c574ae53d5bd13507470c98d53e3b277b588
Andyequisde <andyrevollopro@gmail.com>
2022-08-17 15:40:21 -0300

    avances

--- src/Espuchi.Core/Album.cs
+++ src/Espuchi.Core/Album.cs
@@ -4,20 +4,23 @@ namespace Espuchi.Core
     public class Album
     {
         public ushort IdAlbum { get; set; }
+        public Banda Banda { get; set; }
         public string Nombre { get; set; }
         public DateOnly Lanzamiento { get; set; }
         public int CantRepro { get; set; }
-
-        public Album (ushort idAlbum ,string nombre, DateOnly lanzamiento, int cantRepro)
+        public List<Cancion> Canciones { get; set; }
+        public Album(ushort idAlbum ,string nombre, DateOnly lanzamiento, int cantRepro, Banda banda)
         {
             IdAlbum = idAlbum;
             Nombre = nombre;
             Lanzamiento = lanzamiento;
             CantRepro = cantRepro;
+            Canciones = new List<Cancion>();
+            Banda = banda;
         }
-        public void Canciones()
+        public void AgregarCancion(Cancion cancion)
         {
-            
+            Canciones.Add(cancion);
         }
     }
 }
--- src/Espuchi.Core/Banda.cs
+++ src/Espuchi.Core/Banda.cs
@@ -1,14 +1,22 @@
-﻿namespace Espuchi.Core;
+﻿using System.Collections.Generic;
+
+namespace Espuchi.Core;
 public class Banda
 {
-    public ushort IdBanda { get; set; }
+    public ushort Id { get; set; }
     public string Nombre { get; set; }
     public DateTime Fundacion { get; set; }
+    public List<Album> Albunes { get; set; }
 
-    public Banda (ushort idBanda, string nombre, DateTime fundacion)
+    public Banda (ushort id, string nombre, DateTime fundacion)
     {
-        IdBanda = idBanda;
+        Id = id;
         Nombre = nombre;
         Fundacion = fundacion;
+        Albunes = new List<Album>();
+    }
+    public void AgregarAlbum(Album album)
+    {
+        Albunes.Add(album);
     }
 }
