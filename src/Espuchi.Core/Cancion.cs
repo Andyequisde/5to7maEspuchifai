c9e91bf3ea6b6bb929c10afcc59bada6a5962539
Andyequisde <andyrevollopro@gmail.com>
2022-08-18 15:41:23 -0300

    xd

--- src/Espuchi.Core/Cancion.cs
+++ src/Espuchi.Core/Cancion.cs
@@ -3,16 +3,18 @@ namespace Espuchi.Core
     public class Cancion
     {
         public ushort IdCancion { get; set; }
+        public Album Album { get; set; }
         public string Nombre { get; set; }
         public uint NroOrden { get; set; }
         public int CantRepro { get; set; }
 
-        public Cancion (ushort idCancion, string nombre, uint nroOrden, int cantRepro)
+        public Cancion (ushort idCancion, string nombre, uint nroOrden, int cantRepro, Album album)
         {
             IdCancion = idCancion;
             Nombre = nombre;
             NroOrden = nroOrden;
             CantRepro = cantRepro;
+            Album = album;
         }
     }
 }
