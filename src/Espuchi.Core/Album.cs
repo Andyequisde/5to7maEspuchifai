54d621fba4eeeccc214ed7b8562e936df4f72335
Andyequisde <andyrevollopro@gmail.com>
2022-09-26 10:31:56 -0300

    a

--- src/Espuchi.Core/Album.cs
+++ src/Espuchi.Core/Album.cs
@@ -2,21 +2,20 @@ namespace Espuchi.Core;
 public class Album
 {
     public ushort IdAlbum { get; set; }
-    public Banda Banda { get; set; }
+    public Banda? Banda { get; set; }
     public string Nombre { get; set; }
-    public DateOnly Lanzamiento { get; set; }
+    public DateTime Lanzamiento { get; set; }
     public int CantRepro { get; set; }
     public List<Cancion> Canciones { get; set; }
-    public Album(string nombre, DateOnly lanzamiento, int cantRepro, Banda banda)
+    
+    public Album(string nombre, DateTime lanzamiento, int cantRepro)
     {
         Nombre = nombre;
         Lanzamiento = lanzamiento;
         CantRepro = cantRepro;
         Canciones = new List<Cancion>();
-        Banda = banda;
     }
-    public void AgregarCancion(Cancion cancion)
-    {
-        Canciones.Add(cancion);
-    }
+    public Album(string nombre, DateTime lanzamiento, int cantRepro, Banda? banda = null)
+        : this(nombre, lanzamiento, cantRepro) => Banda = banda;
+    public void AgregarCancion(Cancion cancion) => Canciones.Add(cancion);
 }
